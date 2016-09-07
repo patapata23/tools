946cc70f70ad370870c1936ccfc3adb9e4209a2a
fmy <krswfmy@gmail.com>
2016-09-07 20:49:39 +0900

    add isucon user

--- data/init.sh
+++ data/init.sh
@@ -2,7 +2,11 @@
 
 cd $(dirname $0)
 
+sudo useradd isucon
+
 sudo mkdir -p /home/isucon/.ssh
 sudo chmod 700 /home/isucon/.ssh
-
 sudo cp authorized_keys /home/isucon/.ssh/authorized_keys
+
+sudo chown -R isucon:isucon /home/isucon
+
